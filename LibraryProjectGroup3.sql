1bff8094015508ef2f49ebb14942a0e97bf53b14
zecher <quintondixon@gmail.com>
2018-11-01 14:49:52 -0600

    Finished stored procedure for new asset type

--- LibraryProjectGroup3.sql
+++ LibraryProjectGroup3.sql
@@ -5,3 +5,18 @@ CS 3550
 11/1/18
 */
 
+--You need to create stored procedures to complete the following items:
+--Create new asset types
+CREATE OR ALTER PROCEDURE NewAssetType
+	@newType varchar(50)
+AS
+BEGIN
+	INSERT INTO LibraryProject.AssetTypes
+	(
+		AssetType
+	)
+	VALUES
+		(@newType);
+END;
+
+--Create, update, or deactivate assets
